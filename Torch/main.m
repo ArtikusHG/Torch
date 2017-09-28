ae20c071b611152f459d7834de2999d564e4e265
ipad_kid <ipadkid358@gmail.com>
2017-09-28 06:59:42 -0400

    Unarchived Xcode proj, cleaned up code and Storyboards

---
+++ Torch/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Torch
+//
+//  Created by ArtikusHG on 8/4/17.
+//  Copyright © 2017 ArtikusHG. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
